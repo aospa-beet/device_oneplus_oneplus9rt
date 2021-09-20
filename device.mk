338735262a6ff7334dbb09c21ba080272f274628
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-21 02:21:57 +0300

    marsche: build custom sensors multihal
    Change-Id: Id2883c4b99e36cc6f3b05716f45cf8e5ba73ec5a

--- device.mk
+++ device.mk
@@ -286,7 +286,7 @@ PRODUCT_COPY_FILES += \
     hardware/oplus/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors@2.1-service.oplus-multihal \
     libsensorndkbridge \
     sensors.oplus
 
